d998eb1fc80fd060e991ab7bd736caf850694aeb
Student 8 <Student 8@10.8.33.173>
2016-09-14 13:58:17 -0400

    Suh dude

--- Period 8 Java/src/Introduction/ProceduralExample.java
+++ Period 8 Java/src/Introduction/ProceduralExample.java
@@ -2,6 +2,6 @@ package Introduction;
 
 public class ProceduralExample {
   public static void main(String[]args){  
-    System.out.println("Goodbye, world");
+    System.out.println("Suh dude, world");
   }
 }
