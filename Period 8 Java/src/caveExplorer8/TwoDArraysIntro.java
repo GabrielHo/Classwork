09fc5ade903d0fa90af2bca078545ec4845208f2
Student 8 <Student 8@10.8.33.173>
2016-11-02 14:03:16 -0400

    help me pls

--- Period 8 Java/src/caveExplorer8/TwoDArraysIntro.java
+++ Period 8 Java/src/caveExplorer8/TwoDArraysIntro.java
@@ -5,6 +5,78 @@ import java.util.Arrays;
 public class TwoDArraysIntro {
 
 	public static void main(String[] args) {
+		
+		boolean[][] mines = new boolean[6][6]; //filled with zero because primitive type
+		plantMines(mines);
+		String[][] field = createField(mines);
+		printPic(field);
+		
+	}
+	
+	private static String[][] createField(boolean[][] mines) {
+		String[][] field = new String[mines.length][mines[0].length];
+		for(int row = 0; row < field.length; row++){
+			for(int col = 0; col < field[row].length; col++){
+				if(mines[row][col]){
+					field[row][col] = "x";
+				}
+				else{
+					field[row][col] = countNearby(mines,row,col);
+				}
+			}
+		}
+		
+		return field;
+	}
+
+	private static String countNearby(boolean[][] mines, int row, int col) {
+		for(int r = row-1; r <= row +1; r++){
+			for(int c = col-1; c <= col +1; c++){
+				//check that this element exists
+				if(row >= 0 && r < mines.length && c >=0 && c < mines[0].length){
+					
+				}
+			}
+		}
+		return null;
+	}
+
+	private static void plantMines(boolean[][] mines) {
+		int numberOfMines =10;
+		while(numberOfMines > 0){
+			int row = (int)(Math.random() * mines.length);
+			int col = (int)(Math.random() * mines[0].length);
+			//this prevents the same mine being selected twice
+//			while(mines[row][col]){
+//				row = (int)(Math.random() * mines.length);
+//				col = (int)(Math.random() * mines[0].length);
+//			}
+			if(!mines[row][col]){
+				mines[row][col] = true;
+				numberOfMines --;
+			}
+		}
+		
+	}
+
+
+
+
+
+
+
+
+
+	public static void printPic(String[][] pic){
+		for(String[] row : pic){
+			for(String col :row){
+				System.out.print(col);
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void picMaking(){
 		String[][] pic = new String[10][8];
 		for(int row = 0; row < pic.length; row++){
 			//populate with coordinates
@@ -14,9 +86,9 @@ public class TwoDArraysIntro {
 		}
 		
 		//sun
-		pic[0][3]= "|";
+		pic[0][3]= " |";
 		pic[1][3] = "O";
-		pic[2][3] = "|";
+		pic[2][3] = " |";
 		pic[1][2] = "-";
 		pic[1][4] = "-";
 		//grassy fields
@@ -24,6 +96,18 @@ public class TwoDArraysIntro {
 			for(int col = 0; col <pic[row].length; col++){
 				pic[row][col] = "w";
 			}
+		}
+		
+		//top row and bottom row should be "_"
+		for(int col = 0; col < pic[0].length; col++){
+			pic[0][col] = "_";
+			pic[pic.length-1][col] = "_";
+		}
+		
+		//first and last column is "|"
+		for(int row = 1; row < pic.length; row++){
+			pic[row][0] = "|";
+			pic[row][pic[0].length-1] = "|";
 		}
 		
 		for(String[] row : pic){
