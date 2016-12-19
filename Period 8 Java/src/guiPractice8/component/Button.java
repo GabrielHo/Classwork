41dc540eaf45f9d6c335cf708d94d884c36e4f93
Student 8 <Student 8@10.8.33.173>
2016-12-19 13:45:43 -0500

    yes

--- Period 8 Java/src/guiPractice8/component/Button.java
+++ Period 8 Java/src/guiPractice8/component/Button.java
@@ -1,13 +1,13 @@
 package guiPractice8.component;
 
 import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
 import java.awt.Font;
 import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
 public class Button extends TextLabel implements Clickable{
-	
+
 	private Color color;
 	private Action action;
 	
@@ -16,7 +16,6 @@ public class Button extends TextLabel implements Clickable{
 		this.color = color;
 		this.action = action;
 		update();
-		
 	}
 	
 	public Color getColor(){
@@ -29,37 +28,35 @@ public class Button extends TextLabel implements Clickable{
 	}
 	
 	public void update(Graphics2D g){
-		g = clear();//clears image and gets new graphics
-		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
 		g.setColor(color);
-		g.fillRoundRect(400, 500, 500, 200, 25, 30);
+		g.fillRoundRect(0, 0, getWidth(), getHeight(), 35, 25);
 		g.setColor(Color.black);
-		g.drawRoundRect(400, 500, 500, 200, 25, 30);
+		g.drawRoundRect(0, 0, getWidth()-1, getHeight()-1, 35, 25);
 		g.setFont(new Font(getFont(),Font.PLAIN,getSize()));
 		FontMetrics fm = g.getFontMetrics();
 		
 		if(getText()!= null){
 			g.setColor(Color.white);
 			String t = getText();
+			//just in case text is too wide, cut off
 			int cutoff = t.length();
 			while(cutoff > 0 && fm.stringWidth(t) > getWidth()){
-				cutoff--;
-				t = t.substring(0, cutoff);
+				cutoff --;
+				t = t.substring(0,cutoff); 
 			}
-			g.drawString(t,(getWidth()-fm.stringWidth(t)/2), (getHeight() + fm.getHeight() - fm.getDescent())/2);
+			g.drawString(t, (getWidth()-fm.stringWidth(t))/2, (getHeight()+fm.getHeight()-fm.getDescent())/2);
 		}
-	
 	}
 
-	@Override
+
 	public boolean isHovered(int x, int y) {
-		
-		return false;
+		return x>getX() && x<getX()+getWidth() && y > getY() && y<getY()+getHeight();
 	}
-
-	@Override
-	public void act() {
-		
+	
+	public void act(){
+		action.act();
 	}
 
 }
