24304d49cd661c5b913f68b630323f328e64d02e
Student 8 <Student 8@10.8.33.173>
2016-12-22 13:30:49 -0500

    here you go richard ly

---
+++ Period 8 Java/src/guiPractice8/sampleGames/MyClickableScreen.java
@@ -0,0 +1,56 @@
+package guiPractice8.sampleGames;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+
+import guiPractice8.Screen;
+import guiPractice8.component.Visible;
+
+public class MyClickableScreen extends ClickableScreen implements MouseListener {
+
+	public MyClickableScreen(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void initObjects(ArrayList<Visible> viewObjects) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void initAllObjects(ArrayList<Visible> visibles) {
+		
+	}
+
+}
