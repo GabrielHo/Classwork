fa5959a72fe3bf2cb727d6afe94b20f3383a3882
User <User@Acer>
2016-12-22 22:18:50 -0500

    yes

--- Period 8 Java/src/guiPractice8/whackAMole/WackAMoleScreen.java
+++ Period 8 Java/src/guiPractice8/whackAMole/WackAMoleScreen.java
@@ -2,20 +2,47 @@ package guiPractice8.whackAMole;
 
 import java.util.ArrayList;
 
+import guiPractice8.component.TextLabel;
 import guiPractice8.component.Visible;
 import guiPractice8.sampleGames.ClickableScreen;
 
 public class WackAMoleScreen extends ClickableScreen {
 
+	private ArrayList<MoleInterface> moles;
+	private PlayerInterface player;
+	private TextLabel label;
+	private TextLabel timeLabel;
+	private double timeLeft;
+	 
 	public WackAMoleScreen(int width, int height) {
 		super(width, height);
-		// TODO Auto-generated constructor stub
+		initTimeLeft(60);
+	}
+
+	private void initTimeLeft(int i) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
 	public void initAllObjects(ArrayList<Visible> visibles) {
 		// TODO Auto-generated method stub
+		player = new getAPlayer();
+		moles = new ArrayList();
+	}
+	
+	/**
+	*to implement later, after Character Team implements PlayerInterface
+	*/
+	private PlayerInterface getAPlayer() {
+	 return null;
+	}
 
+	/**
+	*to implement later, after EnemyTeam implements MoleInterface
+	*/
+	private MoleInterface getAMole() {
+	return null;
 	}
 
 }
