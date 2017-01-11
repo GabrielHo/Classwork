dae795d4402820bbe974209a8be5890b390fb2b8
Student 8 <Student 8@10.8.33.173>
2017-01-11 13:56:23 -0500

    xd

--- Period 8 Java/src/guiPractice8/component/MovingComponent.java
+++ Period 8 Java/src/guiPractice8/component/MovingComponent.java
@@ -3,7 +3,7 @@ package guiPractice8.component;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
-public class MovingComponent extends Component implements Runnable {
+public abstract class MovingComponent extends Component implements Runnable {
 	
 	private double vx;
 	private double vy;
@@ -42,17 +42,8 @@ public class MovingComponent extends Component implements Runnable {
 		}
 	}
 
-	public void checkBehaviors() {
-		if(getY()>300){
-			setY(300);
-			vy*=-1;
-		}
-		if(getY()<0){
-			setY(0);
-			vy*=1;
-		}
-	}
-
+	public abstract void checkBehaviors();
+		
 	@Override
 	public void update(Graphics2D g) {
 		long currentTime = System.currentTimeMillis();
@@ -72,12 +63,8 @@ public class MovingComponent extends Component implements Runnable {
 		drawImage(g);
 	}
 	
-	public void drawImage(Graphics2D g) {
-		g.setColor(Color.BLACK);
-		g.fillOval(0, 0, getWidth(), getHeight());
+	public abstract void drawImage(Graphics2D g);
 		
-	}
-
 	public void setX(int x){
 		super.setX(x);
 		posx=x;
--- Period 8 Java/src/guiPractice8/sampleGames/CoordinateScreen.java
+++ Period 8 Java/src/guiPractice8/sampleGames/CoordinateScreen.java
@@ -1,13 +1,18 @@
 package guiPractice8.sampleGames;
 
 import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+
+import javax.swing.ImageIcon;
 
 import guiPractice8.Screen;
 import guiPractice8.component.Action;
+import guiPractice8.component.AnimatedComponent;
 import guiPractice8.component.Button;
 import guiPractice8.component.Graphic;
 import guiPractice8.component.MovingComponent;
@@ -46,11 +51,41 @@ public class CoordinateScreen extends Screen implements MouseMotionListener, Mou
 		viewObjects.add(button);
 		viewObjects.add(picture);
 		
-		MovingComponent mc = new MovingComponent(30,60,80,80);
-		mc.setVy(3);
-		mc.play();
-		viewObjects.add(mc);
+		//MovingComponent mc = new MovingComponent(30,60,80,80);
+		//mc.setVy(3);
+		//mc.play();
+		//viewObjects.add(mc);
+		
+		addAnimation(viewObjects);
 
+	}
+
+	private void addAnimation(ArrayList<Visible> viewObjects) {
+		AnimatedComponent a = new AnimatedComponent(40,50,150,150);
+		try{
+			int numberInRow = 12;
+			int rows =5;
+			int w = 75;
+			int h=60;
+			ImageIcon icon = new ImageIcon("resources/sampleImages/wat.jpg");
+			//create a for loop that will take a sub-image from the sprite grid
+			for(int i = 0; i<numberInRow*rows;i++){
+				//declare the "cropped image"
+				BufferedImage cropped = new BufferedImage(w,h,BufferedImage.TYPE_INT_ARGB);
+				int leftMargin = 0;
+				int topMargin = 0;
+				int x1 = leftMargin + w*(i%numberInRow);
+				int y1 = topMargin + h*(i/numberInRow);
+				Graphics2D g = cropped.createGraphics();
+				g.drawImage(icon.getImage(),0,0,w,h,x1,y1,x1+w,y1+h,null);
+				a.addFrame(cropped, 200);
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		viewObjects.add(a);
+		a.play();
+		
 	}
 
 	public void mouseDragged(MouseEvent arg0) {
